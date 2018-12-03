31f43713fb6c0be6996fdd1188ecc44286360acc
millet.ua.su@gmail.com <millet.ua.su@gmail.com>
2018-12-04 00:06:59 +0200

    Setup infrastructure

---
+++ MfpStore/MfpStore.App/AppServices/Dto/InputDeviceDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MfpStore.App.Models;
+
+namespace MfpStore.App.AppServices.Dto
+{
+    public class InputDeviceDto: BaseModel
+    {
+    }
+}
---
+++ MfpStore/MfpStore.App/Enums/CurrencyEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MfpStore.App.Enums
+{
+    public enum CurrencyEnum
+    {
+        Uah,
+        Rub,
+        Eur,
+        Usd
+    }
+}
