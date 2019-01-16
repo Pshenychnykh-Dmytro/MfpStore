4620a38bbe0373c18ebcfc868e85b1c8c6f129e3
millet.ua.su@gmail.com <millet.ua.su@gmail.com>
2019-01-17 00:25:54 +0200

    fix constants for localization languages

--- MfpStore/MfpStore.App/Constants/GlobalConstants.cs
+++ MfpStore/MfpStore.App/Constants/GlobalConstants.cs
@@ -4,5 +4,9 @@
     {
         public const string DbConnection = "DbConnection";
         public const string DateTimeType = "datetime2";
+        //Allowed route languages
+        public const string Langs = "RU|UK|EN";
+        //Default route language
+        public const string DefaultLang = "RU";        
     }
 }
--- MfpStore/MfpStore.Web/App_Start/RouteConfig.cs
+++ MfpStore/MfpStore.Web/App_Start/RouteConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using MfpStore.App.Constants;
+using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace MfpStore.Web
@@ -7,16 +8,13 @@ namespace MfpStore.Web
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            const string lang = "RU";
-            const string langs = "RU|EN|UK";
-
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
                 name: "Default",
                 url: "{lang}/{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional, lang },
-                constraints: new { lang = langs }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional, lang = GlobalConstants.DefaultLang },
+                constraints: new { lang = GlobalConstants.Langs }
             );
         }
     }
